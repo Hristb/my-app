780973bf60712c45ef3b6fb63d1a150abbe934fa
Joy <hristbartra@gmail.com>
2021-04-20 22:49:03 -0500

    my-app version de prueba

---
+++ src/main/java/joy/myapp/entity/TestEntity.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package joy.myapp.entity;
+
+/**
+ * @author Hrist Joy Bartra Saavedra
+ *
+ */
+public class TestEntity {
+
+	/**
+	 * Propiedad de prueba del archivo de java
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String nombre;
+}
